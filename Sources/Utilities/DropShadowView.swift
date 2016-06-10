6246efb34d4672d3a8fe3bfc55df789ab1b61f20
Merrick Sapsford <merrick@sapsford.tech>
2016-06-10 16:24:03 +0100

    Add gradient layer to DropShadowView

--- Sources/Utilities/DropShadowView.swift
+++ Sources/Utilities/DropShadowView.swift
@@ -10,4 +10,34 @@ import UIKit
 
 internal class DropShadowView: UIView {
     
+    // MARK: Variables
+    
+    private var shadowLayer = CAGradientLayer()
+    
+    // MARK: Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.baseInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.baseInit()
+    }
+    
+    private func baseInit() {
+        
+        shadowLayer.frame = self.bounds
+        shadowLayer.colors = [UIColor.clearColor().CGColor, UIColor.blackColor().colorWithAlphaComponent(0.2).CGColor]
+        self.layer.addSublayer(shadowLayer)
+    }
+    
+    // MARK: Lifecycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        shadowLayer.frame = self.bounds
+    }
 }
