0a141d6dcd93651ba76c9bcacac0064a348923b0
Merrick Sapsford <merrick@sapsford.tech>
2018-01-06 16:59:55 +0100

    Add new framework

---
+++ Sources/CocoaBar/CocoaBar.h
@@ -0,0 +1,19 @@
+//
+//  CocoaBar.h
+//  CocoaBar
+//
+//  Created by Merrick Sapsford on 06/01/2018.
+//  Copyright © 2018 UI At Six. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaBar.
+FOUNDATION_EXPORT double CocoaBarVersionNumber;
+
+//! Project version string for CocoaBar.
+FOUNDATION_EXPORT const unsigned char CocoaBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaBar/PublicHeader.h>
+
+
